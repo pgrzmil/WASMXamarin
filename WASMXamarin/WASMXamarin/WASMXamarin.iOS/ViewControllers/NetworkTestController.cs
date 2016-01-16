2664dab7a99bcc23866db2a24000792e95ab136f
Paweł Grzmil <pawel.grzmil@gmail.com>
2016-01-16 22:46:42 +0100

    Added network test.

--- WASMXamarin/WASMXamarin/WASMXamarin.iOS/ViewControllers/NetworkTestController.cs
+++ WASMXamarin/WASMXamarin/WASMXamarin.iOS/ViewControllers/NetworkTestController.cs
@@ -1,6 +1,8 @@
+using Foundation;
 using System;
-
+using System.Diagnostics;
 using UIKit;
+using Xamarin.Forms;
 
 namespace WASMXamarin.iOS.ViewControllers
 {
@@ -13,15 +15,60 @@ namespace WASMXamarin.iOS.ViewControllers
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
-
-            // Release any cached data, images, etc that aren't in use.
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            Title = "Test obs³ugi sieci";
+            StartButton.TouchUpInside += StartDownloading;
+            NetworkDownloadService.Instance.DownloadCompleted += Instance_DownloadCompleted;
+            //http://i2.kym-cdn.com/photos/images/original/000/581/296/c09.jpg
+            AdressField.Text = "http://cdn.superbwallpapers.com/wallpapers/meme/doge-pattern-27481-2880x1800.jpg";
+        }
 
-            // Perform any additional setup after loading the view, typically from a nib.
+        bool isWorking = true;
+
+        private void StartDownloading(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            ActivityIndicator.Hidden = false;
+            TimeLabel.Hidden = false;
+            StartButton.Hidden = true;
+            AdressField.UserInteractionEnabled = false;
+            
+            Device.StartTimer(TimeSpan.FromMilliseconds(1), () =>
+            {
+                TimeLabel.Text = String.Format("{0}:{1}:{2}", stopwatch.Elapsed.Minutes, stopwatch.Elapsed.Seconds, stopwatch.Elapsed.Milliseconds);
+                return isWorking;
+            });
+
+            NetworkDownloadService.Instance.DownloadImage(AdressField.Text);
+        }
+
+        private void Instance_DownloadCompleted(byte[] bytes)
+        {
+            if (bytes == null)
+                new UIAlertView("B³¹d", "Pobieranie nie powiod³o siê.", null, "OK", null).Show();
+
+            try
+            {
+                var image = new UIImage(NSData.FromArray(bytes));
+                Picture.Image = image;
+            }
+            catch (Exception)
+            {
+                new UIAlertView("B³¹d", "Pobieranie nie powiod³o siê.", null, "OK", null).Show();
+            }
+            finally
+            {
+                ActivityIndicator.Hidden = true;
+                StartButton.Hidden = false;
+                AdressField.UserInteractionEnabled = true;
+                isWorking = false;
+            }
         }
     }
 }
--- WASMXamarin/WASMXamarin/WASMXamarin.iOS/ViewControllers/NetworkTestController.designer.cs
+++ WASMXamarin/WASMXamarin/WASMXamarin.iOS/ViewControllers/NetworkTestController.designer.cs
@@ -14,8 +14,48 @@ namespace WASMXamarin.iOS.ViewControllers
 	[Register ("NetworkTestController")]
 	partial class NetworkTestController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIActivityIndicatorView ActivityIndicator { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField AdressField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView Picture { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton StartButton { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel TimeLabel { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (ActivityIndicator != null) {
+				ActivityIndicator.Dispose ();
+				ActivityIndicator = null;
+			}
+			if (AdressField != null) {
+				AdressField.Dispose ();
+				AdressField = null;
+			}
+			if (Picture != null) {
+				Picture.Dispose ();
+				Picture = null;
+			}
+			if (StartButton != null) {
+				StartButton.Dispose ();
+				StartButton = null;
+			}
+			if (TimeLabel != null) {
+				TimeLabel.Dispose ();
+				TimeLabel = null;
+			}
 		}
 	}
 }
