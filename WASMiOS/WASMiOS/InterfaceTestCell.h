b697ec2b6adc4b097e371be625a5b3575fc3f98e
Pawel Grzmil <pgrzmil@Pawels-MacBook-Pro.local>
2016-01-17 19:21:38 +0100

    Added interface test to ios project

--- WASMiOS/WASMiOS/InterfaceTestCell.h
+++ WASMiOS/WASMiOS/InterfaceTestCell.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface InterfaceTestCell : UITableViewCell
-
+-(void)updateCell:(NSString*)labelText;
 @end
--- WASMiOS/WASMiOS/InterfaceTestCell.m
+++ WASMiOS/WASMiOS/InterfaceTestCell.m
@@ -7,7 +7,54 @@
 //
 
 #import "InterfaceTestCell.h"
+@import AVKit;
+@import AVFoundation;
+
+@interface InterfaceTestCell()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIView *playerView;
+@property (weak, nonatomic) IBOutlet UIImageView *icon;
+
+@end
 
 @implementation InterfaceTestCell
+AVAsset* avAsset;
+AVPlayerItem* avPlayerItem;
+AVPlayerLayer* avPlayerLayer;
+AVPlayer* avPlayer;
+
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"video" ofType:@"mp4"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    avAsset = [AVAsset assetWithURL:url];
+    avPlayerItem =[[AVPlayerItem alloc]initWithAsset:avAsset];
+    avPlayer = [[AVPlayer alloc]initWithPlayerItem:avPlayerItem];
+    avPlayerLayer =[AVPlayerLayer playerLayerWithPlayer:avPlayer];
+    avPlayer.externalPlaybackVideoGravity = AVLayerVideoGravityResizeAspectFill;
+
+    CGRect a= CGRectMake(-105,0, self.frame.size.width-10, self.frame.size.height-40);
+    [avPlayerLayer setFrame:a];
+    [self.playerView.layer addSublayer:avPlayerLayer];
+}
+
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    
+    CABasicAnimation* rotationAnimation;
+    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0 ];
+    rotationAnimation.duration = 1;
+    rotationAnimation.cumulative = YES;
+    rotationAnimation.repeatCount = CGFLOAT_MAX;
+    [self.icon.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+}
+
+-(void)updateCell:(NSString*)labelText{
+    self.label.text = labelText;
+    [avPlayer seekToTime:kCMTimeZero];
+    [avPlayer play];
+}
 
 @end
