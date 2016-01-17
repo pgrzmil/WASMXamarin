756a179e1cfb5ec1e38f0e9372ff54e347ad7ebf
Pawel Grzmil <pgrzmil@Pawels-MacBook-Pro.local>
2016-01-17 12:17:33 +0100

    iOS project started

---
+++ WASMiOS/WASMiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WASMiOS
+//
+//  Created by Pawel Grzmil on 17/01/16.
+//  Copyright © 2016 Pawel Grzmil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WASMiOS/WASMiOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WASMiOS
+//
+//  Created by Pawel Grzmil on 17/01/16.
+//  Copyright © 2016 Pawel Grzmil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
