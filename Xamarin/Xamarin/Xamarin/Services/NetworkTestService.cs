da9b78ff5853d3749a21feb888fdc676bbbfe2dc
Paweł Grzmil <pawel.grzmil@gmail.com>
2016-05-01 19:19:38 +0200

    Total project redesign. Created solution using PCL and Xamarin forms. Addes Shared views and setup for iPhone device deploy.

---
+++ Xamarin/Xamarin/Xamarin/Services/NetworkTestService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Xamarin
+{
+    internal class NetworkDownloadService
+    {
+        public delegate void ImageDownloadEventHandler(byte[] bytes);
+
+        private static readonly NetworkDownloadService instance = new NetworkDownloadService();
+
+        private NetworkDownloadService()
+        {
+        }
+
+        public static NetworkDownloadService Instance { get { return instance; } }
+
+        // public event ImageDownloadEventHandler DownloadCompleted;
+
+        public void DownloadImage(string url)
+        {
+            //TODO:
+            //var webClient = new WebClient();
+            //webClient.DownloadDataCompleted += (s, e) =>
+            //{
+            //    if (DownloadCompleted != null)
+            //    {
+            //        DownloadCompleted(e.Result);
+            //    }
+            //};
+            //webClient.DownloadDataAsync(new Uri(url));
+        }
+    }
+}
